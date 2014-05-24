9421d8eb88eeb93465d13c864daf97d855e3335d
Dito <dhartoto@yahoo.com>
2014-05-24 10:02:10 +1000

    Session routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get  '/register', to: 'users#new', as: 'register'
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get  '/logout', to: 'sessions#destroy'
+
+
   resources :posts, except: :destroy do
     resources :comments, only: :create
   end
-
   resources :categories, except: :destroy
-
+  resources :users, only: [:create, :edit, :update, :show]
 end
